f25b3b2402335c0840f1d65f3ea5caf541cbf3a2
Mau025 <mmoyapalli@gmail.com>
2020-09-10 17:48:30 -0400

    option 2 for transport data

--- src/App.js
+++ src/App.js
@@ -7,21 +7,33 @@ import { Tasks } from './tasks.json';
 /*console.log(Tasks);*/
 
 
-function App() { 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <nav className='navbar navbar-dark bg-dark'>
-          <a href='#' className = 'text-white'>
-            Tasks
-          </a>
-        </nav>
+class App extends Component() {
 
-        <img src={logo} className="App-logo" alt="logo" />
-        
-      </header>
-    </div>
-  ); 
+  constructor(props){
+    super();
+    this.state = {
+      title: 'Aplicacion de tareas',
+      ntareas: 10
+    };
+  }
+
+  
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <nav className='navbar navbar-dark bg-dark'>
+            <a href='#' className = 'text-white'>
+              Tasks
+            </a>
+          </nav>
+  
+          <img src={logo} className="App-logo" alt="logo" />
+          
+        </header>
+      </div>
+    );
+  }  
 }
 
 export default App;
